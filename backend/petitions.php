e13dbd73d50329261ca15a1222b23aae8070543f
Phil <philphoo95@gmail.com>
2015-09-23 20:41:36 -0400

    Re-committing backend folder

---
+++ backend/petitions.php
@@ -0,0 +1,9 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+		//add to the database here
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+		//Generate PHP array, convert to JSON file
+	}
+?>
