a91d8adea771124f5de6200d0e3dce4dd1b72474
Toolbox <jpo3@duke.edu>
2015-10-21 19:44:35 -0400

    Fixed $user objects

--- backend/ensureUser.php
+++ backend/ensureUser.php
@@ -3,37 +3,32 @@
 	require_once("db.php");
 	//creates $conn mysqli instance
 	$user = Array();
-
-	if ($_SERVER['REQUEST_METHOD'] == 'GET'){
 		
-		$query = "SELECT * FROM users WHERE netid=" + explode("@",$_SERVER["eppn"])[0];
+	$query = "SELECT * FROM users WHERE netid='" . explode("@",$_SERVER["eppn"]) . "';";
 
-		//Load all rows in result
-		if ($result = mysqli_query($conn, $query)){
-			if ($user = mysqli_fetch_object($result)){
-				//we're golden. continue.
-			} else {
-				addUser();
-			}
+	//Load all rows in result
+	if ($result = $conn->query($query)){
+		if ($user = mysqli_fetch_object($result)){
+			//we're golden. continue.
 		} else {
-			addUser();
+			addUser($conn);
 		}
-
+	} else {
+		addUser($conn);
 	}
-	
-	function addUser() {
-		$add_user_query = $conn->prepare("INSERT INTO users (netid, admin, remainingvotes, lastvote) VALUES(?,?,?,?)");
-		$add_user_query->bind_param("ss", $netid, $admin);
+		
+	function addUser($conn) {
+		$add_user_query = $conn->prepare("INSERT INTO users (netid, admin, remainingvotes) VALUES(?,?,?)");
+		$add_user_query->bind_param("ssss", $netid, $admin, $remainingvotes);
 		$netid = explode("@",$_SERVER['eppn'])[0];
 		$admin = false;
 		$remainingvotes = 3;
-		$lastvote = 0; //1970
 		$add_user_query->execute();
 		
 		$user["netid"] = $netid;
 		$user["admin"] = $admin;
 		$user["remainingvotes"] = $remainingvotes;
-		$user["lastvote"] = $lastvote;
 	}
-
+	
+	//results in $user object, with properties 'netid','admin','remainingvotes', to be used in other files	
 ?>
