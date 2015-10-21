562cc92c5ccf4637454d860ed0272bd089fae5ea
Phil <philphoo95@gmail.com>
2015-10-21 01:52:23 -0400

    initialized votesleft.php

--- backend/votesLeft.php
+++ backend/votesLeft.php
@@ -1,5 +1,21 @@
 <?php
+	//Server information
+	$host = "localhost";
+	$username = "root";
+	$password = "root";
+	$db_name = "petitions";
 
+	$conn = new mysqli($host, $username, $password, $db_name);
 
+	if ($conn->connect_error){
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+		//Return votes left for a user
+		$user = $_ENV['netid'];
+
+		
+	}
 
 ?>
